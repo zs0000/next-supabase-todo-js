434957ccfacbcacdd7342f6f0b146a044b354ac4
zs0000 <zachsanchez00@gmail.com>
2022-11-26 11:30:40 -0800

    loop animation on home page

--- components/HomeLottie/HomeLottie.js
+++ components/HomeLottie/HomeLottie.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Lottie from "lottie-react";
 import * as f from "../../static/lf20_MW06pA.json";
-const HomeLottie = () => <Lottie animationData={f}/>
+const HomeLottie = () => <Lottie animationData={f} loop={true}/>
 
 export default HomeLottie
